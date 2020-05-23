455c161c16bd544e44a7dbb1e7193924536f163b
Gorigio_liu <l>
2020-05-23 14:21:14 +0800

    初始化计数器项目的基本结构

---
+++ src/components/Addition.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h3>当前最新的count值为：</h3>
+        <button>+1</button>
+    </div>
+</template>
+
+<script>
+</script>
---
+++ src/components/Subtraction.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>当前最新的count值为：</h3>
+        <button>-1</button>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
