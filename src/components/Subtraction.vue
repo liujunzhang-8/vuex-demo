19dd713bbc13175b52964a231a48e021d0a224a9
Gorigio_liu <l>
2020-05-23 17:10:40 +0800

    基于vuex的核心概念的计数器案例

--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
-        <h3>当前最新的count值为：</h3>
-        <button>-1</button>
+        <!-- <h3>当前最新的count值为：{{count}}</h3> -->
+        <h3>{{showNum}}</h3>
+        <button @click="sub">-1</button>
+        <button @click="subN(3)">-N</button>
+        <button @click="subAsync">-1 async</button>
+        <button @click="subNAsync(5)">-N Async</button>
     </div>
 </template>
 <script>
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 export default {
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['showNum'])
+  },
+  methods: {
+    ...mapMutations(['sub', 'subN']),
+    ...mapActions(['subAsync', 'subNAsync'])
+
+  }
 }
 </script>
